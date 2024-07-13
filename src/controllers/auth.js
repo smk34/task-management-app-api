4e5862e94205c3bf857f23e1bb0206ffd8bf8279
Soumik Sarkar <smksarkar5@gmail.com>
2024-07-13 17:15:48 +0530

    mongo-iisue-resolved

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,10 +5,10 @@ import {
   serverErrorResponse,
   successResponse,
   unauthorizedResponse,
-} from "../utils/responses";
-import User from "../models/user";
-import validateUserLogin from "../middlewares/validateUserLogin";
-import { createToken } from "../utils/jwt";
+} from "../utils/responses.js";
+import User from "../models/user.js";
+import validateUserLogin from "../middlewares/validateUserLogin.js";
+import { createToken } from "../utils/jwt.js";
 
 let AuthController = {};
 
@@ -17,7 +17,7 @@ AuthController.resgisterUser = [
     const { email } = req?.body;
     try {
       //Checking for exsiting user with this email
-      const existingUser = User.findOne({ email: email });
+      const existingUser = await User.findOne({ email: email });
       //If yes then restrict the user to resgister with the email
       if (existingUser) {
         return forbiddenResponse(res, {
@@ -28,10 +28,12 @@ AuthController.resgisterUser = [
       //Else allow the user to register
       const creatingUser = new User({ ...req?.body });
       await creatingUser.save();
+
+      // console.log("creaing", creatingUser);
       return successResponse(res, {
         error: false,
         data: { creatingUser },
-        message: "User registered succssesfully!!",
+        message: "User registered succssesfully",
       });
     } catch (error) {
       console.log(error);
@@ -48,12 +50,13 @@ AuthController.userLogin = [
       User.findOne({ email: email })
         .exec()
         .then(async (logindata) => {
-          // console.log("login<><><>",logindata);
+          //Checking for the email in the db
           if (!logindata) {
             return notFoundResponse(res, {
               error: "This email does not exist",
             });
           }
+          //Verification for the password
           if (!logindata.authenticate(password)) {
             return unauthorizedResponse(res, {
               error: "Password do not match",
@@ -61,15 +64,17 @@ AuthController.userLogin = [
           }
           let user = await User.findOne({ email });
           if (user) {
+            //If we found the email then if the user is soft deleted by the admin
             if (user?.isDeleted) {
               return forbiddenResponse(res, {
                 message:
-                  "Your account has been suspended, please contact your adminstrator!!",
+                  "Your account has been suspended, please contact your adminstrator",
               });
+              //The checking that the user have active account or not
             } else if (!user?.isActive) {
               return forbiddenResponse(res, {
                 message:
-                  "Your account has been deactivated, please contact your adminstrator!!",
+                  "Your account has been deactivated, please contact your adminstrator",
               });
             }
           }
@@ -79,6 +84,7 @@ AuthController.userLogin = [
             error: false,
             data: { user },
             authToken,
+            message: "User logged in succssesfully",
           });
         });
     } catch (error) {
